4372933649d47580eb2aad5b624eee9d4bb2f2cb
Mohammed <mohammedaalli088@gmail.com>
2024-07-10 15:24:32 +0000

    feat: functions updated

--- Basics/functions.dart
+++ Basics/functions.dart
@@ -5,7 +5,6 @@ void addNumbers(int num1, int num2) {
   print(num1 + num2);
 }
 
-
 // an int function
 int subNumbers(int a, int b) {
   return (a - b);
@@ -14,4 +13,24 @@ int subNumbers(int a, int b) {
 void main() {
   addNumbers(3, 6);
   print(subNumbers(8, 3));
+  printToConsole();
+  printName("Moali", extraString: "Pundit");
+  print(multiply(28, 3));
+}
+
+// this function does not return anything and do not take any argument
+void printToConsole() {
+  print("Mohammed Ali");
+}
+
+//function with argument
+void printName(String string1, {String? extraString}) {
+  print(string1);
+  print(extraString);
+}
+
+// functions with return types
+
+int multiply(int int1, int int2) {
+  return int1 * int2;
 }
