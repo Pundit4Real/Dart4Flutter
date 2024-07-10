8b1a0f3c022ecd075e421a1e321e8ddb273b021a
Mohammed <mohammedaalli088@gmail.com>
2024-07-10 20:44:04 +0000

    feat: classes and objects done

---
+++ Basics/classes&objects.dart
@@ -0,0 +1,26 @@
+import 'dart:async';
+import 'dart:io';
+
+void main() {
+  var car1 = new Vehicle("v6");
+  Vehicle car2 = new Vehicle('v8');
+  SuperCar car3 = new SuperCar("v16");
+
+  car1.display();
+  print(car2.engine);
+  car3.display();
+}
+
+class Vehicle {
+  String engine = "v8";
+
+  Vehicle(this.engine) {}
+
+  void display() {
+    print(engine);
+  }
+}
+
+class SuperCar extends Vehicle {
+  SuperCar(String engine) : super(engine);
+}
