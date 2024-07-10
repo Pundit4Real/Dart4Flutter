0df179ef199a659a3cace7a6d3acb7963d118042
Mohammed <mohammedaalli088@gmail.com>
2024-07-10 15:10:19 +0000

    feat: numbers done

---
+++ Basics/numbers.dart
@@ -0,0 +1,17 @@
+void main() {
+  int num1 = 10;
+  double num2 = 10.44;
+
+  var num3 = num1 + num2; //same for the basic operators
+  print(num3);
+  var num4 = num1 % num3;
+  print(num4);
+  var a = num.parse("2929");
+  print(a);
+  print(a.isFinite);
+  print(a.isInfinite);
+  print(a.isNaN);
+  print(a.isNegative);
+  print(num1.toDouble());
+  print(a.toString());
+}
