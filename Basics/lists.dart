137e648ca454c5ba718fba0d63be0ff3c9a9c31e
Mohammed <mohammedaalli088@gmail.com>
2024-08-20 20:15:18 +0000

    feat: sets in dart covered

--- Basics/lists.dart
+++ Basics/lists.dart
@@ -1,10 +1,24 @@
 void main() {
+  //declaring list
   var list1 = ['A', 'B', 'C'];
   print(list1);
+  print("-----------------");
+  //adding items to the list
   list1.add("D");
   print(list1);
+  print("-----------------");
+  //creating a list using the List keyword
+  // This way of defining list give us the chance to include all datatypes
+  //the list
+
   List list2 = [2, 'b', 'c'];
   print(list2);
   list2.forEach;
   print(list2);
+  print("-----------------");
+
+  //creating list with List keyword,by specifing the datatype
+  List<String> foods = ['rice', 'banku', 'yam', 'jollof'];
+  print(foods);
+  print(foods[2]);
 }
---
+++ Basics/sets.dart
@@ -0,0 +1,7 @@
+void main() {
+  // set is an unordered collections of unique item in dart
+  // In set duplicate items will be ignored when printed
+
+  Set<int> numbers = {1, 2, 3, 9, 4, 5, 6, 7, 8, 9};
+  print(numbers);
+}
