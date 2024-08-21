ede0ee181736a47236dc3b0ca97981a824924c8d
Mohammed <mohammedaalli088@gmail.com>
2024-08-21 13:52:17 +0000

    feat: type conversion in dart covered

---
+++ Basics/boolean.dart
@@ -0,0 +1,14 @@
+void main() {
+  // booleans in dart yields  onle true or false,
+
+  bool isStudent = true;
+  print(isStudent);
+  bool isMarried = false;
+  bool isWise = true;
+  bool isRich = true;
+  bool areATrader = true;
+  print(isMarried);
+  print(isWise);
+  print(isRich);
+  print(areATrader);
+}
